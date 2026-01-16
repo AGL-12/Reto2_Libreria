aaeb0ed9d6ea51273f31d7313969cc3b83dd834a
Ander719 <ander.arilla99@gmail.com>
2026-01-16 12:39:04 +0100

    Controles carrito ordenado

--- src/controller/ShoppingHistoryController.java
+++ src/controller/ShoppingHistoryController.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package view;
+package controller;
 
+import controller.OrderDetailController;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
